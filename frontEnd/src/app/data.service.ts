040a106f7deaa56620624878a0cfa6ff1d57b6bc
RawanKahlout <36929678+RawanKahlout@users.noreply.github.com>
2020-10-27 16:40:36 +0300

    Update data.service.ts

--- frontEnd/src/app/data.service.ts
+++ frontEnd/src/app/data.service.ts
@@ -8,7 +8,7 @@ export class dataService {
     temp : any;
     constructor(private _http: HttpClient , private _router : Router) { }
     getStatistics(){
-     return this._http.get('http://15.185.60.39:3000/getProductsNew/stats',{
+     return this._http.get('',{
          headers : new HttpHeaders().set('authorization','AIzaSyAJX1z5eViPN6M_-Dzd7XPBQWGBqo-vQt8')
      })
     }
